27440fced0107b00069b9bebb33447cfbba76fca
Vyankatesh Dande <vyankateshdande.vd@gmail.com>
2024-12-31 20:43:01 +0530

    Removed chalk, added more direct exports

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 export { App as default, App, CommandHandler, EventHandler } from "./classes/index.js";
 
 export * as Stores from "./stores/index.js";
+export { MongoStore, ObjectStore} from "./stores//index.js";
+
 export * as Utils from "./utils/index.js";
+export { getPerms } from "./utils/index.js"
